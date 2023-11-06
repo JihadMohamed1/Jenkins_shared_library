0e57ecd8983cbe564768bd765e3681ae2d1c3756
Mohamed jihad <84962456+JihadMohamed1@users.noreply.github.com>
2023-11-06 17:45:30 +0100

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,21 +1,7 @@
 #!/usr/bin/env groovy
 
+package com.example.Docker
+
 def call(String imageName){
-  echo 'build Image stage'
-  withCredentials(
-    [usernamePassword(
-      credentialsId : 'nexus_cred',
-      passwordVariable : 'PASS',
-      usernameVariable : 'USER'
-    )]
-  ){
-    echo "$USER $PASS"
-    sh 'ls'
-    sh "docker build -t $imageName MCS1/."
-      echo "after build"
-    sh "echo $PASS | docker login -u $USER --password-stdin 67.205.176.30:8083"
-     echo "after login"
-    sh "docker push  $imageName"
-     echo "after push"
-  }
+return new Docker(this).buildDockerImage(imageName)
 }
